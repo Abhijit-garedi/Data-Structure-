ee0af4543a6fc475ac6085d80649e39e511ed851
Abhijit-garedi <paljustwin@gmail.com>
2024-05-05 21:43:56 +0530

    1a program 1 to n numbers

---
+++ Week 1/1a_print_1_to_n.c
@@ -0,0 +1,22 @@
+// A c program to print 1 to n number without using any loop
+
+ #include <stdio.h> 
+  void print(int i,  int n)
+   {
+    if(i <= n)
+    {
+     printf("%d\t", i);
+    print(i+1, n);
+    }
+   }
+
+   int main()
+   {
+    int n;
+    printf("enter a number:");
+    scanf("%d", &n);
+    
+    printf("Number upto %d are :", n);
+    print(1, n);
+    return 0;
+   }
